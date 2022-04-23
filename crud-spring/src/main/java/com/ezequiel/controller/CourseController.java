0f7616d1016241b28c9af55a6c42cf676cac4a94
EzequielHubertoSchuh <ezequiel_utf@yahoo.com.br>
2022-04-23 14:41:27 -0300

    update

--- crud-spring/src/main/java/com/ezequiel/controller/CourseController.java
+++ crud-spring/src/main/java/com/ezequiel/controller/CourseController.java
@@ -5,21 +5,21 @@ import java.util.List;
 import com.ezequiel.model.Course;
 import com.ezequiel.repository.CourseRepository;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.*;
 
 import lombok.AllArgsConstructor;
-
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/courses")
 @AllArgsConstructor
+@Api(value="API REST Forum")
 public class CourseController {
 
     private final CourseRepository courseRepository;
 
-    //@RequestMapping(method = RequestMethod.GET)
+    @ApiOperation(value="Retorna uma lista de Cursos")  //@RequestMapping(method = RequestMethod.GET)
     @GetMapping
     public @ResponseBody List<Course> list() {
         return courseRepository.findAll();
