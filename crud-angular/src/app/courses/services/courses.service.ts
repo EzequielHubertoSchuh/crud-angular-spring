c3198a78511533947abe2fdd1fb34fcc3f26baae
EzequielHubertoSchuh <ezequiel_utf@yahoo.com.br>
2022-04-30 11:24:44 -0300

    post

--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -5,20 +5,22 @@ import { Course } from '../model/course';
 import { delay, first, tap } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CoursesService {
-
   private readonly API = 'api/courses';
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
   list() {
-    return this.httpClient.get<Course[]>(this.API)
-    .pipe(
+    return this.httpClient.get<Course[]>(this.API).pipe(
       first(),
       // delay(5000),
-      tap(courses => console.log(courses))
+      tap((courses) => console.log(courses))
     );
+  }
+
+  save(record: Course) {
+    return this.httpClient.post<Course>(this.API, record).pipe(first());
   }
 }
