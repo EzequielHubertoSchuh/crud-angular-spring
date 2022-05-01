984c29f0e02880d240b6579062dca673b07ff0ba
EzequielHubertoSchuh <ezequiel_utf@yahoo.com.br>
2022-05-01 00:56:58 -0300

    update pom.xml

--- crud-spring/src/main/java/com/ezequiel/CrudSpringApplication.java
+++ crud-spring/src/main/java/com/ezequiel/CrudSpringApplication.java
@@ -15,16 +15,4 @@ public class CrudSpringApplication {
 		SpringApplication.run(CrudSpringApplication.class, args);
 	}
 
-	@Bean
-	CommandLineRunner initDatabase(CourseRepository courseRepository) {
-		return args -> {
-			courseRepository.deleteAll();
-
-			Course c = new Course();
-			c.setName("Angular com Spring");
-			c.setCategory("front-end");
-
-			courseRepository.save(c);
-		};
-	}
 }
