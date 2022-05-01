a26fc834d5b1c6cce6a476291b55ddeb3360751e
EzequielHubertoSchuh <ezequiel_utf@yahoo.com.br>
2022-05-01 08:43:46 -0300

    hateoas

--- crud-spring/src/main/java/com/ezequiel/controller/CourseController.java
+++ crud-spring/src/main/java/com/ezequiel/controller/CourseController.java
@@ -1,12 +1,15 @@
 package com.ezequiel.controller;
 
+import java.awt.*;
 import java.util.List;
+import java.util.Optional;
 
 import com.ezequiel.model.Course;
 import com.ezequiel.repository.CourseRepository;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -21,48 +24,67 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 @RestController
 @RequestMapping("/api/courses")
 @AllArgsConstructor
-@Api(value="API REST Cursos")
+@Api(value = "API REST Cursos")
 public class CourseController {
 
-    private final CourseRepository courseRepository;
+    @Autowired
+    CourseRepository courseRepository;
 
-    @ApiOperation(value="Retorna uma lista de Cursos")  //@RequestMapping(method = RequestMethod.GET)
+    @ApiOperation(value = "Retorna uma lista de Cursos")
     @GetMapping
-    public ResponseEntity<List<Course>> list (){
+    public ResponseEntity<List<Course>> list() {
         List<Course> courseList = courseRepository.findAll();
-        if(courseList.isEmpty()){
+        if (courseList.isEmpty()) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }else {
-            for(Course curso : courseList) {
-                long id = curso.getId();
-                curso.add(linkTo(methodOn(CourseController.class).listaProdutoUnico(id)).withSelfRel());
+        } else {
+            for (Course course : courseList) {
+                long id = course.getId();
+                course.add(linkTo(methodOn(CourseController.class).listaCursoUnico(id)).withSelfRel());
             }
             return new ResponseEntity<List<Course>>(courseList, HttpStatus.OK);
         }
     }
 
-    @ApiOperation(value="Retorna um curso unico")
+    @ApiOperation(value = "Retorna um curso unico")
     @GetMapping("/{id}")
-    public Course listaProdutoUnico(@PathVariable(value="id") long id){
-        return courseRepository.findById(id);
+    public ResponseEntity<Course> listaCursoUnico(@PathVariable(value = "id") long id) {
+        Optional<Course> courseO = Optional.ofNullable(courseRepository.findById(id));
+        if (!courseO.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else {
+            courseO.get().add(linkTo(methodOn(CourseController.class).list()).withRel("Lista de cursos"));
+            return new ResponseEntity<Course>(courseO.get(), HttpStatus.OK);
+        }
     }
 
-    @ApiOperation(value="Salva um curso")
+    @ApiOperation(value = "Salva um curso")
     @PostMapping
-    public Course salvaProduto(@RequestBody @Validated Course curso) {
-        return courseRepository.save(curso);
+    public ResponseEntity<Course> salvaCurso(@RequestBody @Validated Course course) {
+        return new ResponseEntity<Course>(courseRepository.save(course), HttpStatus.CREATED);
     }
 
-    @ApiOperation(value="Deleta um curso")
-    @DeleteMapping
-    public void deletaProduto(@RequestBody @Validated Course curso) {
-        courseRepository.delete(curso);
+    @ApiOperation(value = "Deleta um curso")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deletaCurso(@PathVariable(value = "id") long id){
+        Optional<Course> courseO = Optional.ofNullable(courseRepository.findById(id));
+        if (!courseO.isPresent()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }else {
+            courseRepository.delete(courseO.get());
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
     }
 
-    @ApiOperation(value="Atualiza um curso")
-    @PutMapping
-    public Course atualizaProduto(@RequestBody @Validated Course curso) {
-        return courseRepository.save(curso);
+    @ApiOperation(value = "Atualiza um curso")
+    @PutMapping("/{id}")
+    public ResponseEntity<Course> updateProduto(@PathVariable(value="id") long id,
+                                                      @RequestBody @Validated Course course) {
+        Optional<Course> courseO = Optional.ofNullable(courseRepository.findById(id));
+        if(!courseO.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        course.setId(courseO.get().getId());
+        return new ResponseEntity<Course>(courseRepository.save(course), HttpStatus.OK);
     }
 
 }
