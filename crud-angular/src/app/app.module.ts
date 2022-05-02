b2687cbfdb2d3b2a2a5d2f40fcc6ccc1ff148c00
EzequielHubertoSchuh <ezequiel_utf@yahoo.com.br>
2022-05-01 21:55:19 -0300

    update

--- crud-angular/src/app/app.module.ts
+++ crud-angular/src/app/app.module.ts
@@ -6,10 +6,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home/home.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
